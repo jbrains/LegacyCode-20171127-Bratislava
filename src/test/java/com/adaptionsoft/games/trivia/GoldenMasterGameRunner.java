99f50a480cd72887f3b885b5cf1ff9af1740ee9e
J. B. Rainsberger <me@jbrains.ca>
2017-11-18 14:12:11 +0200

    We now generate a new Golden Master only when no Golden Master exists.

--- src/test/java/com/adaptionsoft/games/trivia/GoldenMasterGameRunner.java
+++ src/test/java/com/adaptionsoft/games/trivia/GoldenMasterGameRunner.java
@@ -17,19 +17,26 @@ public class GoldenMasterGameRunner {
     public static void main(String[] args) throws Exception {
         final FileSystem fileSystem = FileSystems.getDefault();
         final Path goldenMasterRoot = fileSystem.getPath("src", "test", "data", "golden-master");
+        final Path goldenMasterRunRoot = fileSystem.getPath("src", "test", "data", "golden-master-run");
         if (!fileSystem.isOpen())
             throw new RuntimeException("The golden master file system is not open.");
 
-        final File goldenMasterRootAsFile = goldenMasterRoot.toFile();
-        goldenMasterRootAsFile.mkdirs();
-        if (!goldenMasterRootAsFile.exists())
-            throw new RuntimeException(String.format("I couldn't create the golden master directory at path [%s]", goldenMasterRoot));
+        final File goldenMasterRootAsFile = ensureDirectoryExists(goldenMasterRoot);
+        final File goldenMasterRunRootAsFile = ensureDirectoryExists(goldenMasterRunRoot);
 
         final long gameSeed = 812736L;
 
-        final File goldenMasterFile = new File(goldenMasterRootAsFile, String.format("game-%d.txt", gameSeed));
+        final String gameOutputFileName = String.format("game-%d.txt", gameSeed);
+        final File goldenMasterFile = new File(goldenMasterRootAsFile, gameOutputFileName);
 
-        System.setOut(new PrintStream(goldenMasterFile));
+        final File gameOutputFile;
+        if (goldenMasterFile.exists()) {
+            gameOutputFile = new File(goldenMasterRunRootAsFile, gameOutputFileName);
+        } else {
+            gameOutputFile = goldenMasterFile;
+        }
+
+        System.setOut(new PrintStream(gameOutputFile));
 
         Game aGame = new Game();
 
@@ -52,5 +59,13 @@ public class GoldenMasterGameRunner {
 
         } while (notAWinner);
 
+    }
+
+    private static File ensureDirectoryExists(final Path goldenMasterRoot) {
+        final File goldenMasterRootAsFile = goldenMasterRoot.toFile();
+        goldenMasterRootAsFile.mkdirs();
+        if (!goldenMasterRootAsFile.exists())
+            throw new RuntimeException(String.format("I couldn't create the golden master directory at path [%s]", goldenMasterRoot));
+        return goldenMasterRootAsFile;
     }
 }
