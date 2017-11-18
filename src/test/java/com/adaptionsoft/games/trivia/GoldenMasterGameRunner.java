7f63fd4d4baf2f2af68cc43983b2ea416a1a740d
J. B. Rainsberger <me@jbrains.ca>
2017-11-18 14:06:41 +0200

    We now generate a single golden master game.

--- src/test/java/com/adaptionsoft/games/trivia/GoldenMasterGameRunner.java
+++ src/test/java/com/adaptionsoft/games/trivia/GoldenMasterGameRunner.java
@@ -2,6 +2,11 @@ package com.adaptionsoft.games.trivia;
 
 import com.adaptionsoft.games.uglytrivia.Game;
 
+import java.io.File;
+import java.io.PrintStream;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 import java.util.Random;
 
 
@@ -9,7 +14,19 @@ public class GoldenMasterGameRunner {
 
     private static boolean notAWinner;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        final FileSystem fileSystem = FileSystems.getDefault();
+        final Path goldenMasterRoot = fileSystem.getPath("src", "test", "data", "golden-master");
+        if (!fileSystem.isOpen())
+            throw new RuntimeException("The golden master file system is not open.");
+
+        final File goldenMasterRootAsFile = goldenMasterRoot.toFile();
+        goldenMasterRootAsFile.mkdirs();
+        if (!goldenMasterRootAsFile.exists())
+            throw new RuntimeException(String.format("I couldn't create the golden master directory at path [%s]", goldenMasterRoot));
+
+        System.setOut(new PrintStream(new File(goldenMasterRootAsFile, String.format("game-%d.txt", 812736L))));
+
         Game aGame = new Game();
 
         aGame.add("Chet");
