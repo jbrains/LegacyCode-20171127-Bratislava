e0112e90cbaed0ebc9ac533fadb97905aacb49d2
J. B. Rainsberger <me@jbrains.ca>
2017-11-28 13:37:37 +0100

    askQuestion() now uses the purer-but-not-quite-pure function.

--- src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
+++ src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
@@ -136,14 +136,16 @@ public class Game {
     }
 
     private void askQuestion() {
-        if (currentCategory() == "Pop")
-            System.out.println(popQuestions.removeFirst());
-        if (currentCategory() == "Science")
-            System.out.println(scienceQuestions.removeFirst());
-        if (currentCategory() == "Sports")
-            System.out.println(sportsQuestions.removeFirst());
-        if (currentCategory() == "Rock")
-            System.out.println(rockQuestions.removeFirst());
+        try {
+            System.out.println(
+                    chooseNextQuestionInCategory(
+                            currentCategory(),
+                            popQuestions,
+                            scienceQuestions,
+                            sportsQuestions,
+                            rockQuestions));
+        } catch (IllegalStateException intentionallyDoNothingJustAsWeDidBefore) {
+        }
     }
 
     private String currentCategory() {
