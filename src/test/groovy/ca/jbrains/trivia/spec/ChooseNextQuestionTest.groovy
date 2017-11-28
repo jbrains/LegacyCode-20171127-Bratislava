f56a4e57a8ca6a358a899985523fec8b89ed63e7
J. B. Rainsberger <me@jbrains.ca>
2017-11-28 13:28:08 +0100

    We now confirm that we can choose the correct next question, when there is one.

---
+++ src/test/groovy/ca/jbrains/trivia/spec/ChooseNextQuestionTest.groovy
@@ -0,0 +1,32 @@
+package ca.jbrains.trivia.spec
+
+import com.adaptionsoft.games.uglytrivia.Game
+import spock.lang.Specification
+import spock.lang.Unroll
+
+
+class ChooseNextQuestionTest extends Specification {
+    private def fakeNextQuestionIn(categoryName) {
+        String.format("::the next %s question::", categoryName)
+    }
+
+    @Unroll
+    def "choose the next question in the deck for category #category"() {
+        given:
+        def popQuestions = new LinkedList([fakeNextQuestionIn("Pop")])
+        def scienceQuestions = new LinkedList([fakeNextQuestionIn("Science")])
+        def sportsQuestions = new LinkedList([fakeNextQuestionIn("Sports")])
+        def rockQuestions = new LinkedList([fakeNextQuestionIn("Rock")])
+
+        expect:
+        Game.chooseNextQuestionInCategory(category, popQuestions, scienceQuestions, sportsQuestions, rockQuestions) == expectedNextQuestion
+
+        where:
+        category  | expectedNextQuestion
+        "Pop"     | fakeNextQuestionIn("Pop")
+        "Science" | fakeNextQuestionIn("Science")
+        "Sports"  | fakeNextQuestionIn("Sports")
+        "Rock"    | fakeNextQuestionIn("Rock")
+    }
+
+}
